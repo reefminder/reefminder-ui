d36b15d38c0207a663bf3a783c7e377fc9f0a2d0
Bingham <dbinh@ad.allstate.com>
2016-06-01 14:08:53 +0100

    Adding placeholder image for dev

--- client/src/app/components/layout/header/header.jsx
+++ client/src/app/components/layout/header/header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import placeholderImg from '../../../assets/placeholder.svg';
 
 class Header extends React.Component {
 
     render() {
         return <div>
                 <nav className="navbar navbar-dark bg-inverse header-height header">
-                <a className="navbar-brand" href="#">ReefMinder</a>
+                <a className="navbar-brand" href="#">
+                    <img className="icon-placeholder" src={ placeholderImg }/>
+                    ReefMinder
+                </a>
             </nav>
         </div>;
     }
