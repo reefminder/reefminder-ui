f0539b1f0ef0529e207db4e71f3fc8c86347ba87
David Bingham <david.r.bingham@gmail.com>
2016-06-02 18:46:34 +0100

    Added Image

--- client/src/app/components/layout/header/header.jsx
+++ client/src/app/components/layout/header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import placeholderImg from '../../../assets/placeholder.svg';
+import placeholderImg from '../../../../../assets/placeholder.svg';
 
 class Header extends React.Component {
 
@@ -9,7 +9,6 @@ class Header extends React.Component {
                 <nav className="navbar navbar-dark bg-inverse header-height header">
                 <a className="navbar-brand" href="#">
                     <img className="icon-placeholder" src={ placeholderImg }/>
-                    ReefMinder
                 </a>
             </nav>
         </div>;
