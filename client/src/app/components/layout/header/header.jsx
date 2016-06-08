834ce0c8c38256d8cc0b8b634e56b1d886d5c68a
Bingham <dbinh@ad.allstate.com>
2016-06-08 12:07:31 +0100

    Updating from bootstrap 4 to React-Toolbox

--- client/src/app/components/layout/header/header.jsx
+++ client/src/app/components/layout/header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import placeholderImg from '../../../assets/placeholder.svg';
+import placeholderImg from '../../../../../assets/placeholder.svg';
 
 class Header extends React.Component {
 
