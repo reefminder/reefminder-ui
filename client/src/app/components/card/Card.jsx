94c9c52a7fc64420b39c7cd47606079abfe8828a
David Bingham <david.r.bingham@gmail.com>
2016-06-20 18:39:02 +0100

    Ongoing chart updates

--- client/src/app/components/card/Card.jsx
+++ client/src/app/components/card/Card.jsx
@@ -7,18 +7,30 @@ import { Line } from 'react-chartjs-2';
 
 class Card extends React.Component {
 
-    static chartData = {
-        label: 'Scatter Dataset',
-        data: [{
-            x: -10,
-            y: 0
-        }, {
-            x: 0,
-            y: 10
-        }, {
-            x: 10,
-            y: 5
-        }
+    static data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }
         ]
     };
 
@@ -34,7 +46,7 @@ class Card extends React.Component {
                     aspectRatio="wide"
                     image="https://placeimg.com/800/450/nature"
                 />
-                <Line data={Card.chartData}/>
+                <Line data={ Card.data }/>
                 <CardText>Some Dummy Text</CardText>
                 <CardActions theme={ cardTheme }>
                     <Button label="Action 1" theme={ buttonTheme } />
